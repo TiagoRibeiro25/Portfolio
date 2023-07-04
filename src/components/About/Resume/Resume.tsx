e2ce6e7eeca2925f331cb194c231feec09b896b5
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-07-04 15:58:09 +0100

    refactor: Updated resume

--- src/components/About/Resume/Resume.tsx
+++ src/components/About/Resume/Resume.tsx
@@ -39,7 +39,7 @@ export default function Resume() {
 						{iconSvg}
 						Download Resume
 					</button>
-					<span className="mt-5 text-sm text-gray-500">Last Updated: 27th June 2023</span>
+					<span className="mt-5 text-sm text-gray-500">Last Updated: 04th July 2023</span>
 				</main>
 			</div>
 		</div>
